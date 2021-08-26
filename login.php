f57ef8ab586d90c5dba022d444c4467364276bcf
mrsSGhash <63438240+mrsSGhash@users.noreply.github.com>
2021-08-26 13:54:27 +1200

    Add files via upload
    forgot the div class="row"

--- login.php
+++ login.php
@@ -59,23 +59,25 @@
 </head>
 <body>
     <div class="containter">
-        <div class="col-md-12">
-            <h2>Login</h2>
-            <p>Please fill in your email and password.</p>
-            <form action="" method="post">
-                <div class="form-group">
-                    <label>Email Address</label>
-                    <input type="email" name="email" class="form-control" required />
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" name="password" class="form-control" required />
-                </div>
-                <div class="form-group">
-                    <input type="submit" name="submit" class="btn bnt-primary" value="Submit">
-                </div>
-                <p>Don't have an account? <a hred="register.php">Register here</a>.</p>
-            </form>
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Login</h2>
+                <p>Please fill in your email and password.</p>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label>Email Address</label>
+                        <input type="email" name="email" class="form-control" required />
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" required />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="submit" class="btn bnt-primary" value="Submit">
+                    </div>
+                    <p>Don't have an account? <a hred="register.php">Register here</a>.</p>
+                </form>
+            </div>
         </div>
     </div>
 </body>
